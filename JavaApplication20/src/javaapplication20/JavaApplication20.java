9034a63869c6a0d3c183d384c484ac2c3c95f9d9
YiCi Kek <YiCi Kek@YiCiKek>
2024-04-22 15:19:56 +0800

    commit kek

--- JavaApplication20/src/javaapplication20/JavaApplication20.java
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -14,7 +14,8 @@ public class JavaApplication20 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic heres
+        System.out.println("hellow world");
     }
     
 }
